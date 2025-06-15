0647ec5193b47c653ff01065f990f24f54823b90
Ditya <thoriqk2@gmail.com>
2025-06-15 18:55:28 +0700

    chore: edit some wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,23 +1,25 @@
 -- ~/.wezterm.lua
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 return {
-  -- Konfigurasi font
-  font = wezterm.font 'CaskaydiaCove Nerd Font',
-  font_size = 11.0, -- Sesuaikan dengan preferensi Anda
+	-- Konfigurasi font
+	font = wezterm.font("CaskaydiaCove Nerd Font"),
+	font_size = 11.0, -- Sesuaikan dengan preferensi Anda
 
-  -- Konfigurasi warna (Gruvbox)
-  color_scheme = "Gruvbox dark, hard (base16)", -- Atau "Gruvbox light, hard (base16)" untuk tema terang
+	-- Konfigurasi warna (Gruvbox)
+	color_scheme = "Gruvbox dark, hard (base16)", -- Atau "Gruvbox light, hard (base16)" untuk tema terang
 
-  -- Pengaturan umum terminal
-  adjust_window_size_when_changing_font_size = false,
-  enable_tab_bar = true,
-  enable_wayland = true,
-  window_background_opacity = 0.5,
+	-- Pengaturan umum terminal
+	adjust_window_size_when_changing_font_size = false,
+	enable_tab_bar = true,
+	enable_wayland = true,
+	window_background_opacity = 0.5,
+	window_decorations = "NONE",
 
-  -- Ukuran baris tab
-  tab_bar_at_bottom = false,
-  use_fancy_tab_bar = true,
-  hide_tab_bar_if_only_one_tab = true,
+	-- Ukuran baris tab
+	tab_bar_at_bottom = false,
+	use_fancy_tab_bar = true,
+	hide_tab_bar_if_only_one_tab = true,
 }
+
