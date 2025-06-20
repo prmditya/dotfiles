7b9ef8aa217fdee96c44777d8f6f2e6e0c9324c0
prmditya <thoriqk2@gmail.com>
2025-06-20 20:03:50 +0700

    chore: adding lazyvim config

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,8 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- better escape
+map("i", "jj", "<ESC>", { desc = "Other Switch to Normal Mode", silent = true })
