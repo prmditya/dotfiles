9c405491f459b0f2df0a4df3173f29220a67afb8
prmditya <thoriqk2@gmail.com>
2025-07-02 19:00:39 +0700

    fix(pywal): changing from pywal to pywal16

--- scripts/scripts/pick-wallpaper.sh
+++ scripts/scripts/pick-wallpaper.sh
@@ -34,12 +34,10 @@ for MON in $MONITORS; do
 done
 
 # Generate pywal theme
-wal -i "$WALL"
+wal --cols16 lighten -i "$WALL"
 nvim --headless "+lua require('lushwal').generate()" +qa
 sed -i "s|^ *path = .*|    path = $WALL|" ~/.config/hypr/hyprlock.conf
-~/.config/hypr/update-hypr-colors.sh
 ~/.config/dunst/update-dunst-colors.sh
-~/.config/ghostty/update-ghostty-colors.sh
 
 # Reload components
 killall waybar && waybar &
