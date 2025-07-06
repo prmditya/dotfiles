9d1e760226b68227702e48ec1689c18e02d41862
prmditya <thoriqk2@gmail.com>
2025-07-06 21:33:43 +0700

    style(suckless): organize suckless software

--- scripts/scripts/dwmbar.sh
+++ scripts/scripts/dwmbar.sh
@@ -4,9 +4,10 @@ while true; do
   temp="$(/home/ditya/.config/waybar/avg-temp.sh)"
   volume="$(pamixer --get-volume)%"
   brightness=$(brightnessctl -m | cut -d',' -f4)
-  battery="$(acpi -b | grep '^Battery 1' | awk -F', ' '{print $2}')"
+  battery1="$(acpi -b | grep '^Battery 0' | awk -F', ' '{print $2}')"
+  battery2="$(acpi -b | grep '^Battery 1' | awk -F', ' '{print $2}')"
   date="$(date '+%a %d %b %H:%M')"
 
-  xsetroot -name "$temp | 󰕾 $volume | 󰃟 $brightness |  $battery | $date"
+  xsetroot -name "$temp | 󰕾 $volume | 󰃟 $brightness |  $battery1: $battery2 | $date"
   sleep 5
 done
